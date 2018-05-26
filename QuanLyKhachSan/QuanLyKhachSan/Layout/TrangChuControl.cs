e5d40925708f3ded32a9294828dcf07482b1678b
Quang Hải <Quang Hi@DESKTOP-SSCJKUR>
2018-05-27 02:10:01 +0700

    Design, Object, Connection

---
+++ QuanLyKhachSan/QuanLyKhachSan/Layout/TrangChuControl.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan.Layout
+{
+    public partial class TrangChuControl : UserControl
+    {
+       
+        public TrangChuControl()
+        {
+            InitializeComponent();
+            
+        }
+        private void lb1(object sender, EventArgs e)
+        {
+           
+        }
+        private void lb2(object sender, EventArgs e)
+        {
+            EventKTTang(2);
+        }
+        private void lb3(object sender, EventArgs e)
+        {
+            EventKTTang(3);
+        }
+        private void lb4(object sender, EventArgs e)
+        {
+            EventKTTang(4);
+        }
+        private void lb5(object sender, EventArgs e)
+        {
+            EventKTTang(5);
+        }
+        private void lb6(object sender, EventArgs e)
+        {
+            EventKTTang(6);
+        }
+        private void lb7(object sender, EventArgs e)
+        {
+            EventKTTang(7);
+        }
+        private void lb8(object sender, EventArgs e)
+        {
+            EventKTTang(8);
+        }
+        private void lb9(object sender, EventArgs e)
+        {
+            EventKTTang(9);
+        }
+        private void lb10(object sender, EventArgs e)
+        {
+            EventKTTang(10);
+        }
+        private void lb11(object sender, EventArgs e)
+        {
+            EventKTTang(11);
+        }
+        private void lb12(object sender, EventArgs e)
+        {
+            EventKTTang(12);
+        }
+        private void lb13(object sender, EventArgs e)
+        {
+            EventKTTang(13);
+        }
+        private void lb14(object sender, EventArgs e)
+        {
+            EventKTTang(14);
+        }
+        private void lb15(object sender, EventArgs e)
+        {
+            EventKTTang(15);
+        }
+        private void Display(int tang)
+        {
+           
+
+        }
+        private void DDTang_Select(object sender, EventArgs e)
+        {
+            
+        }
+        private void EventKTTang(int a)
+        {
+            
+        }
+
+        private void btdattraphong_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btlammoi_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
